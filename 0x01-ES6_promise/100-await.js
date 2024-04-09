e3585cd3e687b63a26199527a8621a034a59cca5
spaceadh <spacesealadhesives13@gmail.com>
2024-04-09 09:23:23 +0300

    Advanced Promises

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  let res = {};
+
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
+    res = { photo, user };
+  } catch (err) {
+    res = { photo: null, user: null };
+  }
+  return res;
+}
