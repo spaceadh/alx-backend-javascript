a537d67ee5ac398f93d01449f60e326388644f92
spaceadh <spacesealadhesives13@gmail.com>
2024-04-10 07:03:00 +0300

    correction1

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -2,15 +2,13 @@
 
 /* eslint-disable no-unused-vars */
 /* eslint-disable prefer-promise-reject-errors */
-
-/*
- * Always keep every promises you make
- * And make only promises you can keep
- * even if that means making empty promises
-*/
 export default function getResponseFromAPI() {
-    return new Promise((res, rej) => {
-      res('Hi Mom');
-      rej('Rejected');
-    });
-  }
+  const success = true;
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve();
+    } else {
+      reject();
+    }
+  });
+}
